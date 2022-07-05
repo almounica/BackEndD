d2464bcf641ddbaa70c0cfa3609342ec3c48100e
almounica <108088289+almounica@users.noreply.github.com>
2022-07-06 00:56:27 +0530

    Add files via upload

---
+++ service/EmployeeManagementService.java
@@ -0,0 +1,11 @@
+package com.mypackage.service;
+
+import java.util.ArrayList;
+
+public interface EmployeeManagementService {
+	void displayEmp(ArrayList<Employee> e);
+	void deleteEmp(ArrayList<Employee> e) throws MyException;
+	void updateEmp(ArrayList<Employee> e) throws MyException;
+	void  addEmp(ArrayList<Employee> e);
+	void searchEmp(ArrayList<Employee> e) throws MyException;
+}
