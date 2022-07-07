2d097d7712b01f5d51ac49c5736514a7696c5629
almounica <108088289+almounica@users.noreply.github.com>
2022-07-07 15:33:12 +0530

    Update ArraySortRank.java

--- ArraySortRank.java
+++ ArraySortRank.java
@@ -1,72 +1,85 @@
 package com.mypackage.service;
 
 import java.util.ArrayList;
-
-class Values
+import java.util.PriorityQueue;
+import java.util.Scanner;
+class Num
 {
-	 int value;
-
-     public Values(int value)
-     {
-         this.value=value;
-     }
-     public String toString() {
-    	 return " "+this.value;
-     }
+	int num;
+	public Num(int num) {
+		this.num=num;
+	}
+	public String toString() {
+        return  " " +this.num;
+        }
 }
-class Ranks
+class Value implements Comparable<Value>
 {
-	  int rank;
-
-	    public Ranks(int rank)
-	    {
-	        this.rank=rank;
-	    }
-	    public String toString() {
-	    	 return " "+this.rank;
-	     }
+	int value;
+	public Value(int value)
+	{
+		this.value=value;
+	}
+	@Override
+	public int compareTo(Value o) {
+		if(value<o.value)
+            return -1;
+        else if(value>o.value)
+            return 1;
+		return 0;
+	}
+	
+}
+class RankValue
+{
+	int value; int rank;
+	public RankValue(int value,int rank) {
+		this.value=value;
+		this.rank=rank;	
+	}
+	
+}
+class Rank 
+{
+	int rank;
+	public Rank(int rank) {
+		this.rank=rank;
+	}
+	public String toString() {
+        return  " " +this.rank;
+        }
 }
 public class ArraySortRank {
 
 	public static void main(String[] args) {
-		ArrayList<Values> v=new ArrayList<>();
-        ArrayList<Ranks> r=new ArrayList<>();
-        v.add(new Values(10));
-        v.add(new Values(15));
-        v.add(new Values(16));
-        v.add(new Values(8));
-        v.add(new Values(9));
-        v.add(new Values(2));
-        v.add(new Values(3));
-        v.add(new Values(6));
-        v.add(new Values(1));
-System.out.println(v);
-       
-  int len=v.size();
-        while(!v.isEmpty()) 
-        while(len>0) 
-
-        {
-            for(Values i: v) {
-                int rank=1;
-
-                for(Values j:v) {
-                    if(i.value>j.value )
-                    {
-                        rank=rank+1;
-
-                    }    
-                }
-                r.add(new Ranks(rank));
-
-                len=len-1;
-
-        } 
-           System.out.println(r);
-
-        }
-
-    }
+		ArrayList<Num> n =new ArrayList<>();
+		 PriorityQueue<Value> v =new PriorityQueue<>();
+		 ArrayList<RankValue> i = new ArrayList<>();
+		 ArrayList<Rank> rank = new ArrayList<>();
+		   Scanner sc =new Scanner(System.in);
+	        System.out.println("enter numbers");
+			while(sc.hasNextInt()) {
+				n.add(new Num(sc.nextInt()));
+			}
+			System.out.println(n);
+			for(Num k:n) {
+				v.add(new Value(k.num));
+			}
+	        int r=1;
+	        while(!v.isEmpty()) {
+	        	 Value j=v.poll();
+	        	i.add(new RankValue(j.value,r));
+	        	r=r+1;
+	        }
+	        for(Num a:n) {
+	        	for(RankValue b:i) {
+	        		if(a.num==b.value) {
+	        			rank.add(new Rank(b.rank));
+	        		}
+	        	}
+	        }
+	      System.out.println(rank);  
+	}
 
 }
 
