eda42902e3112d2de3e8f78bff1965dc1489c447
almounica <108088289+almounica@users.noreply.github.com>
2022-07-07 08:39:12 +0530

    Add files via upload

---
+++ ArraySortRank.java
@@ -0,0 +1,93 @@
+package com.mypackage.service;
+
+import java.util.ArrayList;
+
+class Values
+{
+	 int value;
+
+     public Values(int value)
+     {
+         this.value=value;
+     }
+     public String toString() {
+    	 return " "+this.value;
+     }
+}
+class ValueRanks
+{
+	 int value;
+     int rank;
+
+     public ValueRanks(int value,int rank)
+     {
+         this.value=value;
+         this.rank=rank;
+
+     }
+}
+class Ranks
+{
+	  int rank;
+
+	    public Ranks(int rank)
+	    {
+	        this.rank=rank;
+	    }
+	    public String toString() {
+	    	 return " "+this.rank;
+	     }
+}
+public class ArraySortRank {
+
+	public static void main(String[] args) {
+		ArrayList<Values> v=new ArrayList<>();
+        ArrayList<ValueRanks> vr =new ArrayList<>();
+        ArrayList<Ranks> r=new ArrayList<>();
+
+        v.add(new Values(10));
+        v.add(new Values(15));
+        v.add(new Values(16));
+        v.add(new Values(8));
+        v.add(new Values(9));
+        v.add(new Values(2));
+        v.add(new Values(3));
+        v.add(new Values(6));
+        v.add(new Values(1));
+System.out.println(v);
+       
+  int len=v.size();
+        while(!v.isEmpty()) 
+        while(len>0) 
+
+        {
+            for(Values i: v) {
+                int rank=1;
+
+                for(Values j:v) {
+                    if(i.value>j.value )
+                    {
+                        rank=rank+1;
+
+                    }    
+                }
+                vr.add(new ValueRanks(i.value,rank));
+
+                len=len-1;
+
+        }
+
+            for(ValueRanks i:vr) {
+
+                   r.add(new Ranks(i.rank));
+
+            }    
+           System.out.println(r);
+
+        }
+
+    }
+
+	}
+
+
