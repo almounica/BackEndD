5e504862d50a2f8f2b15d77271bf253fd8453477
almounica <108088289+almounica@users.noreply.github.com>
2022-07-12 16:04:37 +0530

    Update TestMain.java

--- main/TestMain.java
+++ main/TestMain.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TestMain {
 
-	public static void main(String[] args) throws TestException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter string 1");
 		String string1=sc.next();
@@ -19,11 +19,13 @@ public class TestMain {
 	    	System.out.println(s.handleException(a));
 	    }catch(TestException e){
 	    	System.out.println(e.getMessage());
+	    }
+           try {
+         System.out.println(s.doOperation(a));
 	    }catch(OperatorException e2)
 	    {
 	    	System.out.println(e2.getMessage());
 	    }
-         System.out.println(s.doOperation(a));
 	    
 	    }
 	 
