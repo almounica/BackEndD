d5b88d2ca485244fe0607cb86d91b9f4b0792b60
almounica <108088289+almounica@users.noreply.github.com>
2022-07-14 09:22:53 +0530

    Add files via upload

---
+++ CompareVariables.java
@@ -0,0 +1,15 @@
+public class CompareVariables {
+	public static <H extends Comparable<H>> H highest(H var1,H var2,H var3) {
+		H max =var1;
+		if(var2.compareTo(max)>0)
+			max=var2;
+		if(var3.compareTo(max)>0)
+			max=var3;
+		return max;
+	}
+	public static void main(String[] args) {
+		System.out.println(highest(3,7,3));
+        System.out.println(highest("mouni","mou","mounica"));
+        System.out.println(highest(1.1,3.3,0.5));
+	}
+}
